089e1e81208996d1498451145dbbdb7c3f61e6b8
Alexey Bondarev <alexeybondarev45@gmail.com>
2020-11-15 20:39:23 +0300

    added remote origin

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Test {
+	public static void main(String[] args) {
+		HashMap<String, Integer> objectObjectHashMap = new HashMap<>();
+		objectObjectHashMap.put("1", 1);
+		objectObjectHashMap.put("2", 2);
+		objectObjectHashMap.get("1");
+
+		if (true) {
+			System.out.println(1);
+		} else {
+			System.out.println(2);
+		}
+	}
+}
